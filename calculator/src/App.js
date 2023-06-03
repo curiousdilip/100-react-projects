b4c3feba05db5cd5248478d4972e74c5ed6622fc
Dilip Kumar <curiousdilip@gmail.com>
2023-06-04 01:07:29 +0530

    evaluate function completed

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -34,7 +34,39 @@ switch(type){
         currentOperand:null,
       }
     }
+
+    return {
+      ...state,
+      previousOperand:evaluate(state),
+      operation:payload.operation,
+      currentOperand:null
+    }
 }
+}
+
+function evaluate({
+  currentOperand,previousOperand, operation
+}){
+  const prev=parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if(isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  switch (operation){
+    case "+":
+      computation=prev+current
+      break
+      case "-":
+      computation=prev-current
+      break
+      case "/":
+      computation=prev/current
+      break
+      case "*":
+      computation=prev*current
+      break
+      
+  }
+  return computation.toString()
 }
 function App() {
   const [{currentOperand,previousOperand,operation  }, dispatch]=useReducer(reducer,{})
